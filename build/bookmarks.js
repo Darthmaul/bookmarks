c52988cc6e3b52da5d23d072c69a5a6d8b6bdfa8
Fergus Ruston <fergus.ruston@gmail.com>
2016-11-17 01:23:43 +0100

    default models

--- build/bookmarks.js
+++ build/bookmarks.js
@@ -51291,7 +51291,7 @@
 	var defaultLists = [{
 		id: "15gqtphky",
 		bookmarks: ['waduj9le7', '7p9uamiy5', 'di8untgx8'],
-		description: 'This is a list. It contains all the default bookmarks. \n\n You are reading the list description. \n\n *Descriptions can use Markdown.* Take a look at this [link to see Markdown syntax](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet).',
+		description: 'This is a list. It contains all the default bookmarks. \n\n You are reading the list description. \n\n *Descriptions can use Markdown.* Take a look at this [link to see Markdown syntax](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet). \n\n Try editing this list and reordering the bookmarks it contains!',
 		slug: "default-bookmarks-list",
 		title: "Default bookmarks list"
 	}];
