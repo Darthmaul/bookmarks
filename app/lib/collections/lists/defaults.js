042f9fa578d995d15203770f8b7c4524cc307d0f
Fergus Ruston <fergus.ruston@gmail.com>
2016-11-17 01:24:24 +0100

    default models

--- app/lib/collections/lists/defaults.js
+++ app/lib/collections/lists/defaults.js
@@ -2,7 +2,7 @@ const defaultLists = [
 	{
 		id: "15gqtphky",
 		bookmarks: ['waduj9le7', '7p9uamiy5', 'di8untgx8'],
-		description: 'This is a list. It contains all the default bookmarks. \n\n You are reading the list description. \n\n *Descriptions can use Markdown.* Take a look at this [link to see Markdown syntax](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet). \n\n Try editing this list and reordering the bookmarks it contains!',
+		description: 'This is a list. It contains all the default bookmarks. \n\n You are reading the list description. \n\n *Descriptions can use Markdown.* Take a look at this [link to see Markdown syntax](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet). \n\n --- \n\n Try editing this list and reordering the bookmarks it contains!',
 		slug: "default-bookmarks-list",
 		title: "Default bookmarks list"
 	}
