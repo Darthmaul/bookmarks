613571b2b020ec7032c4a6955ab8eb413a2ab6ed
Fergus Ruston <fergus.ruston@gmail.com>
2016-11-20 23:09:51 +0100

    default bookmarks

--- app/lib/collections/bookmarks/defaults.js
+++ app/lib/collections/bookmarks/defaults.js
@@ -4,7 +4,7 @@ const defaultBookmarks = [
 		title: 'Some of my photography',
 		url: 'https://c4.staticflickr.com/1/773/22353007131_8cf5803bc8_k.jpg',
 		domain: 'www.flickr.com',
-		description: "This is a default bookmark. In this case it's a bookmark to an image. You can view the link directly by clicking the domain name below the image. \n\n Below the domain is a list of tags associated with this bookmark. \n\n To the right of the domain are toggles that allow you to change how the bookmark is displayed. \n\n You are currently reading the bookmark description, *and it supports markdown!* \n\n If you haven't already, try creating a new bookmark by clicking the plus symbol at the top right of the page. \n\n --- \n\n *See some of my other photography on [Flickr](http://flickr.com/photos/fergusruston).*",
+		description: "This is a default bookmark. In this case it's a bookmark to an image. You can view the link directly by clicking the domain name below the image. \n\n Below the domain is a list of tags associated with this bookmark. \n\n To the right of the domain is a toggle that allows you to view options to change how the bookmark is displayed. \n\n You are currently reading the bookmark description, *and it supports markdown!* \n\n If you haven't already, try creating a new bookmark by clicking the plus symbol at the top right of the page. \n\n --- \n\n *See some of my other photography on [Flickr](http://flickr.com/photos/fergusruston).*",
 		tags: ['default bookmark', 'photography'],
 		slug: 'some-of-my-photography',
 		date: new Date
